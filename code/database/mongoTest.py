d98ee1e1a808e84c83e5557dee8ee198cefec3ed
Alexander Hull <hullale@oregonstate.edu>
2019-01-17 17:04:47 -0800

    more testing

--- code/database/mongoTest.py
+++ code/database/mongoTest.py
@@ -1,7 +1,83 @@
 from pymongo import MongoClient
-from pprint import pprint
+
+class Condition:
+    def __init__(self, typeDamage, primaryDamageLocation, secondaryDamageLocation, startCode, keyFOB, wheels, airbags):
+        self.typeDamage = typeDamage
+        self.primaryDamageLocation = primaryDamageLocation
+        self.secondaryDamageLocation = secondaryDamageLocation
+        self.startCode = startCode
+        self.keyFOB = keyFOB
+        self.wheels = wheels
+        self.airbags = airbags
+
+class Vehicle:
+    def __init__(self, vin, make, model, series, year, bodyStyle, transmission, exteriorColor, odometer, condition):
+        self.vin = vin
+        self.make = make
+        self.model = model
+        self.series = series
+        self.year = year
+        self.bodyStyle = bodyStyle
+        self.transmission = transmission
+        self.exteriorColor = exteriorColor
+        self.odometer = odometer
+        self.condition = condition #condition object
+
+class Database:
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.client = MongoClient(host, port)
+    
+    def establishConnection(self):
+        self.client = MongoClient(self.host, self.port)
+        self.db = self.client.admin
+        self.posts = self.db.posts
+
+    def addVehicle(self, vehicle):
+        post_data = {
+            'vin': vehicle.vin,
+            'make': vehicle.make,
+            'model': vehicle.model,
+            'series': vehicle.series,
+            'year': vehicle.year,
+            'bodyStyle': vehicle.bodyStyle,
+            'transmission': vehicle.transmission,
+            'exteriorColor': vehicle.exteriorColor,
+            'odometer': vehicle.odometer,
+            'typeDamage': vehicle.condition.typeDamage,
+            'primaryDamageLocation': vehicle.condition.primaryDamageLocation,
+            'secondaryDamageLocation': vehicle.condition.secondaryDamageLocation,
+            'startCode': vehicle.condition.startCode,
+            'keyFOB': vehicle.condition.keyFOB,
+            'wheels': vehicle.condition.wheels,
+            'airbags': vehicle.condition.airbags
+        }
+        result = self.posts.insert_one(post_data)
+        return result 
+
+    def getVehicle(self, vin):
+        retrieve_post = self.posts.find_one({"vin": vin})
+        return retrieve_post
 
 def main():
+    database = Database("localhost", 27017)
+    database.establishConnection()
+
+    condition1 = Condition("Collision", "Right Side", "None", "Runs", "Present", "Standard Wheels and spare present", "Deployed")
+    vehicle1 = Vehicle(1234512345, "Ford", "Focus", "SE", 2012, "Hatchback", "Standard", "Red", 34242, condition1)
+
+    result = database.addVehicle(vehicle1)
+    print(result)
+    print('One post: {0}'.format(result.inserted_id))
+
+    result = database.getVehicle("1234512345")
+    print(result)
+    print('One post: {0}'.format(result.inserted_id))
+
+def main2():
+
     #Establish connection
     client = MongoClient("localhost", 27017)
     #Access Database
@@ -16,7 +92,7 @@ def main():
         'damage': 'lots'
     }
     result = posts.insert_one(post_data)
-    pprint(result)
+    print(result)
     print('One post: {0}'.format(result.inserted_id))
 
     post_1 = {
