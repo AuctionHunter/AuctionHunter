608a7feec0dcf84b5c794a85e2142117d14d6ea6
Hull <shs_hullale@onid.oregonstate.edu>
2019-04-10 14:29:24 -0700

    price scraping test

--- code/crawler/Keystone project/tutorial/tutorial/spiders/iaaibot.py
+++ code/crawler/Keystone project/tutorial/tutorial/spiders/iaaibot.py
@@ -8,66 +8,46 @@ class RedditbotSpider(scrapy.Spider):
 
     def parse_page(self, reponse):
         #parse info for particular auction entry page
-        CarName = 
-        CarImage =
-        Miles = 
-        #Vin = response.xpath("//*[@id=\"VIN_vehicleStats1\"]/text()")
-        PrimaryDamage = 
-        price = response.css("ul.list-sale-info")
-        price.xpath("/li/span")
-        #Give the extracted content row wise
-        scraped_info = {
-            'car name' : item[0],
-			'miles':item[1],
-			'vin':item[2],
-			'primary damage':item[3],
-			'car image' : item[4], #Set's the url for scrapy to download images	
-        }
+        estimated_price = response.xpath("//ul[contains(@class, 'list-sale-info')]//li//span//text()")[3].extract()
+        repair_costs = response.xpath("//ul[contains(@class, 'list-sale-info')]//li//span//text()")[5].extract()
+
+        price_info = [estimated_price,repair_costs]
         #yield or give the scraped info to scrapy
-        return scraped_info
+        return price_info
 
     def parse(self, response):
         #Extracting the content using css selectors
-        next_button = response.css("ul.pagination flexitem")
+        CarName = response.css('#dvSearchList h4::text').getall()
+        CarImage = response.css('.lazy').getall()
+        Miles = response.css('td:nth-child(3) p~ p+ p::text').getall()
+        Vin = response.css('a~ p:nth-child(3)::text').getall()
+        PrimaryDamage = response.css('td:nth-child(5) p:nth-child(2)::text').getall()
 
-
+        #link parsing
         table = response.css('table.table')
         rows = table.xpath("//tbody//tr")
-
-        #Right click on element, copy, copy xpath 
-        #//*[@id="dvSearchList"]/div[1]/div[5]/div/ul/li[13]
-        #"//*[@id=\"dvSearchList\"]/div[1]/div[5]/div/ul/li[13]/@href"
 
         for entry in rows:
             url = entry.xpath("td//@href")[1].extract()
 
             if url is not None:
-                yield response.follow(url, self.parse_page)
+                price_info += response.follow(url, self.parse_page)
 
-
-            #go to that url and parse 
-            #if next_page is not None:
-            #    yield response.follow(next_page, callback=self.parse)
-
-
-
-        #CarName = response.css('#dvSearchList h4::text').getall()
-        #CarImage = response.css('.lazy').getall()
-        #Miles = response.css('td:nth-child(3) p~ p+ p::text').getall()
-        #Vin = response.css('a~ p:nth-child(3)::text').getall()
-        #PrimaryDamage = response.css('td:nth-child(5) p:nth-child(2)::text').getall()
         #Give the extracted content row wise
+        i = 0
         for item in zip(CarName,Miles,Vin,PrimaryDamage,CarImage):
             #create a dictionary to store the scraped info
             scraped_info = {
                 'car name' : item[0],
-				'miles':item[1],
-				'vin':item[2],
-				'primary damage':item[3],
-				'car image' : item[4], #Set's the url for scrapy to download images
-				
+                'miles':item[1],
+                'vin':item[2],
+                'primary damage':item[3],
+                'car image' : item[4], #Set's the url for scrapy to download images
+                'estimated price' : price_info[i],
+                'repair costs' : price_info[i+1]
             }
-
+            if((i+1)<len(price_info)):
+                i+=2
             #yield or give the scraped info to scrapy
             yield scraped_info
 
