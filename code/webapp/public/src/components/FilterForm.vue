5ef01f2718988c8a3afad720dc7563384c008d95
steelee <esteele101@gmail.com>
2019-05-03 15:48:13 -0700

    Added more filters - also rounded value coming out of dataEnhancer.py

--- code/webapp/public/src/components/FilterForm.vue
+++ code/webapp/public/src/components/FilterForm.vue
@@ -1,9 +1,74 @@
 <template>
-<div style="margin-right:1%;">
+<div style="margin-right:1%; margin-left:1%;">
   <h3>Filter Options</h3>
   <hr />
   <h4>Vehicle name search</h4>
   <input v-model="filterKey" placeholder="Vehicle name" />
+  <hr />
+  <h4>Airbags</h4>
+  <select v-model="airbagKey">
+    <option disabled value="">Select an option</option>
+    <option v-for="airbagOption in airbagOptions">
+      {{airbagOption}}
+    </option>
+  </select>
+  <hr />
+  <h4>Body Type</h4>
+  <select v-model="bodyKey">
+    <option disabled value="">Select an option</option>
+    <option v-for="bodyOption in bodyOptions">
+      {{bodyOption}}
+    </option>
+  </select>
+  <hr />
+  <h4>Key Fob</h4>
+  <select v-model="fobKey">
+    <option disabled value="">Select an option</option>
+    <option v-for="fobOption in fobOptions">
+      {{fobOption}}
+    </option>
+  </select>
+  <hr />
+  <h4>Start Code</h4>
+  <select v-model="codeKey">
+    <option disabled value="">Select an option</option>
+    <option v-for="codeOption in codeOptions">
+      {{codeOption}}
+    </option>
+  </select>
+  <hr />
+  <h4>Price</h4>
+  <select v-model="priceKey">
+    <option disabled value="">Select an option</option>
+    <option>
+      High to Low
+    </option>
+    <option>
+      Low to High
+    </option>
+  </select>
+  <hr />
+  <h4>Miles</h4>
+  <select v-model="mileKey">
+    <option disabled value="">Select an option</option>
+    <option>
+      High to Low
+    </option>
+    <option>
+      Low to High
+    </option>
+  </select>
+  <hr />
+  <h4>Estimated Value</h4>
+  <select v-model="valueKey">
+    <option disabled value="">Select an option</option>
+    <option>
+      High to Low
+    </option>
+    <option>
+      Low to High
+    </option>
+  </select>
   <hr />
   <ResetButton></ResetButton>
 </div>
@@ -15,25 +80,103 @@ export default {
   name: 'FilterForm',
   data() {
     return {
-      filterKey: this.message
+      filterKey: this.message,
+      airbagKey: this.airbags,
+      bodyKey: this.bodys,
+      fobKey: this.fobs,
+      priceKey: this.price,
+      mileKey: this.miles,
+      codeKey: this.codes,
+      valueKey: this.values,
+      airbagOptions: [],
+      bodyOptions: [],
+      fobOptions: [],
+      codeOptions: []
     }
   },
   created: function() {
     var parent = this;
     this.$eventHub.$on('clearSearchForms', function(payload) {
       parent.filterKey = "";
+      parent.airbagKey = "";
+      parent.bodyKey = "";
+      parent.fobKey = "";
+      parent.codeKey = "";
+      parent.priceKey = "";
+      parent.mileKey = "";
+      parent.valueKey = "";
+    });
+    this.$eventHub.$on('setAirbagSearchOptions', function(payload) {
+      parent.airbagOptions = payload;
+    });
+    this.$eventHub.$on('setBodySearchOptions', function(payload) {
+      parent.bodyOptions = payload;
+    });
+    this.$eventHub.$on('setFobSearchOptions', function(payload) {
+      parent.fobOptions = payload;
+    });
+    this.$eventHub.$on('setCodeSearchOptions', function(payload) {
+      parent.codeOptions = payload;
     });
   },
   props: {
     message: String,
+    airbags: String,
+    bodys: String,
+    fobs: String,
+    price: String,
+    miles: String,
+    values: String,
+    codes: String
   },
   components: {
     ResetButton
   },
   watch: {
-    filterKey: function(newVal, oldVal) { // watch it
+    filterKey: function(newVal, oldVal) {
       this.$eventHub.$emit('searchName', this.filterKey);
+    },
+    airbagKey: function(newVal, oldVal) {
+      if (newVal) {
+        this.$eventHub.$emit('searchAirbag', this.airbagKey);
+      }
+    },
+    bodyKey: function(newVal, oldVal) {
+      if (newVal) {
+        this.$eventHub.$emit('searchBody', this.bodyKey);
+      }
+    },
+    fobKey: function(newVal, oldVal) {
+      if (newVal) {
+        this.$eventHub.$emit('searchFob', this.fobKey);
+      }
+    },
+    codeKey: function(newVal, oldVal) {
+      if (newVal) {
+        this.$eventHub.$emit('searchCode', this.codeKey);
+      }
+    },
+    priceKey: function(newVal, oldVal) {
+      if (newVal) {
+        this.$eventHub.$emit('sortPrice', this.priceKey);
+        this.mileKey = "";
+        this.valueKey = "";
+      }
+    },
+    mileKey: function(newVal, oldVal) {
+      if (newVal) {
+        this.$eventHub.$emit('sortMiles', this.mileKey);
+        this.priceKey = "";
+        this.valueKey = "";
+      }
+    },
+    valueKey: function(newVal, oldVal) {
+      if (newVal) {
+        this.$eventHub.$emit('sortValue', this.valueKey);
+        this.priceKey = "";
+        this.mileKey = "";
+      }
     }
-  },
+  }
 }
 </script>
--- code/webapp/public/src/components/ListItems.vue
+++ code/webapp/public/src/components/ListItems.vue
@@ -1,7 +1,12 @@
 <template>
 <div class="card-columns">
   <div v-for="(auctionItem, index) in auctionItems">
-    <DataCard :index="trackIndex(index)" :carName="auctionItem.car_name" :carThumbnail="auctionItem.car_image" :data="auctionItem">
+    <DataCard :index="trackIndex(index)"
+    :carName="auctionItem.car_name"
+    :carThumbnail="auctionItem.car_image"
+    :data="auctionItem"
+    :total="auctionItems.length"
+    >
     </DataCard>
   </div>
 </div>
@@ -16,7 +21,14 @@ export default {
   data() {
     return {
       auctionItems: [],
-      masterList: []
+      masterList: [],
+      airbagOptionsList: [],
+      bodyOptionsList: [],
+      fobOptionsList: [],
+      codeOptionsList: [],
+      sortedPrice: false,
+      sortedMiles: false,
+      sortedValue: false
     }
   },
   components: {
@@ -31,9 +43,45 @@ export default {
     this.$eventHub.$on('searchName', function(payload) {
       parent.auctionItems = parent.nameSearch(payload.toUpperCase(), parent.auctionItems);
     });
+    this.$eventHub.$on('searchAirbag', function(payload) {
+      parent.auctionItems = parent.airbagSearch(payload, parent.auctionItems);
+    });
+    this.$eventHub.$on('searchBody', function(payload) {
+      parent.auctionItems = parent.bodySearch(payload, parent.auctionItems);
+    });
+    this.$eventHub.$on('searchFob', function(payload) {
+      parent.auctionItems = parent.fobSearch(payload, parent.auctionItems);
+    });
+    this.$eventHub.$on('searchCode', function(payload) {
+      parent.auctionItems = parent.codeSearch(payload, parent.auctionItems);
+    });
+    this.$eventHub.$on('sortPrice', function(payload) {
+      parent.sortedPrice = true;
+      this.$eventHub.$emit('toggleMiles', false);
+      this.$eventHub.$emit('toggleValue', false);
+      parent.auctionItems = parent.sortPrice(payload, parent.auctionItems);
+    });
+    this.$eventHub.$on('sortMiles', function(payload) {
+      parent.sortedMiles= true;
+      this.$eventHub.$emit('togglePrice', false);
+      this.$eventHub.$emit('toggleValue', false);
+      parent.auctionItems = parent.sortMiles(payload, parent.auctionItems);
+    });
+    this.$eventHub.$on('sortValue', function(payload) {
+      parent.sortedValue= true;
+      this.$eventHub.$emit('togglePrice', false);
+      this.$eventHub.$emit('toggleMiles', false);
+      parent.auctionItems = parent.sortValue(payload, parent.auctionItems);
+    });
     this.$eventHub.$on('resetSearch', function(payload) {
       parent.auctionItems = parent.masterList;
+      parent.sortedPrice = false;
+      parent.sortedMiles = false;
+      parent.sortedValue = false;
       this.$eventHub.$emit('clearSearchForms');
+      this.$eventHub.$emit('togglePrice', false);
+      this.$eventHub.$emit('toggleMiles', false);
+      this.$eventHub.$emit('toggleValue', false);
     });
   },
   methods: {
@@ -50,11 +98,157 @@ export default {
       }
       return resultArray;
     },
+    airbagOptions(entries) {
+      var flags = [],
+        output = [],
+        l = entries.length,
+        i;
+      for (i = 0; i < l; i++) {
+        if (flags[entries[i].airbags]) continue;
+        flags[entries[i].airbags] = true;
+        output.push(entries[i].airbags);
+      }
+      return output;
+    },
+    airbagSearch(airbagKey, myArray) {
+      var resultArray = [];
+      for (var i = 0; i < myArray.length; i++) {
+        var curr_string = myArray[i].airbags
+        if (curr_string == airbagKey) {
+          resultArray.push(myArray[i]);
+        }
+      }
+      return resultArray;
+    },
+    bodyOptions(entries) {
+      var flags = [],
+        output = [],
+        l = entries.length,
+        i;
+      for (i = 0; i < l; i++) {
+        if (flags[entries[i].body]) continue;
+        flags[entries[i].body] = true;
+        output.push(entries[i].body);
+      }
+      return output;
+    },
+    bodySearch(bodyKey, myArray) {
+      var resultArray = [];
+      for (var i = 0; i < myArray.length; i++) {
+        var curr_string = myArray[i].body
+        if (curr_string == bodyKey) {
+          resultArray.push(myArray[i]);
+        }
+      }
+      return resultArray;
+    },
+    codeOptions(entries) {
+      var flags = [],
+        output = [],
+        l = entries.length,
+        i;
+      for (i = 0; i < l; i++) {
+        if (flags[entries[i].start_code]) continue;
+        flags[entries[i].start_code] = true;
+        output.push(entries[i].start_code);
+      }
+      return output;
+    },
+    codeSearch(codeKey, myArray) {
+      var resultArray = [];
+      for (var i = 0; i < myArray.length; i++) {
+        var curr_string = myArray[i].start_code
+        if (curr_string == codeKey) {
+          resultArray.push(myArray[i]);
+        }
+      }
+      return resultArray;
+    },
+    fobOptions(entries) {
+      var flags = [],
+        output = [],
+        l = entries.length,
+        i;
+      for (i = 0; i < l; i++) {
+        if (flags[entries[i].key_fob]) continue;
+        flags[entries[i].key_fob] = true;
+        output.push(entries[i].key_fob);
+      }
+      return output;
+    },
+    fobSearch(fobKey, myArray) {
+      var resultArray = [];
+      for (var i = 0; i < myArray.length; i++) {
+        var curr_string = myArray[i].key_fob
+        if (curr_string == fobKey) {
+          resultArray.push(myArray[i]);
+        }
+      }
+      return resultArray;
+    },
+    comparePrices(a, b) {
+      if (parseInt(a.estimated_price, 10) < parseInt(b.estimated_price, 10)) {
+        return -1;
+      }
+      if (parseInt(a.estimated_price, 10) > parseInt(b.estimated_price, 10)) {
+        return 1;
+      }
+      return 0;
+    },
+    sortPrice(dir, myArray) {
+      var sortedArray = [];
+      this.$eventHub.$emit('togglePrice', true);
+      sortedArray = myArray.sort(this.comparePrices);
+      if (dir == "High to Low"){
+        sortedArray = sortedArray.reverse()
+      }
+      return sortedArray;
+    },
+    compareMiles(a, b) {
+      if (parseInt(a.miles.replace(/,/g,""), 10) < parseInt(b.miles.replace(/,/g,""), 10)) {
+        return -1;
+      }
+      if (parseInt(a.miles.replace(/,/g,""), 10) > parseInt(b.miles.replace(/,/g,""), 10)) {
+        return 1;
+      }
+      return 0;
+    },
+    sortMiles(dir, myArray) {
+      var sortedArray = [];
+      this.$eventHub.$emit('toggleMiles', true);
+      sortedArray = myArray.sort(this.compareMiles);
+      if (dir == "High to Low"){
+        sortedArray = sortedArray.reverse()
+      }
+      return sortedArray;
+    },
+    compareValue(a, b) {
+      if (a.value_est < b.value_est) {
+        return -1;
+      }
+      if (a.value_est > b.value_est) {
+        return 1;
+      }
+      return 0;
+    },
+    sortValue(dir, myArray) {
+      var sortedArray = [];
+      this.$eventHub.$emit('toggleValue', true);
+      sortedArray = myArray.sort(this.compareValue);
+      if (dir == "High to Low"){
+        sortedArray = sortedArray.reverse()
+      }
+      return sortedArray;
+    },
     fetchItems() {
       let uri = 'http://localhost:8080/api/all';
       axios.get(uri).then((response) => {
         this.auctionItems = response.data;
         this.masterList = response.data;
+        this.$eventHub.$emit('setAirbagSearchOptions', this.airbagOptions(response.data));
+        this.$eventHub.$emit('setBodySearchOptions', this.bodyOptions(response.data));
+        this.$eventHub.$emit('setFobSearchOptions', this.fobOptions(response.data));
+        this.$eventHub.$emit('setCodeSearchOptions', this.codeOptions(response.data));
       });
     }
   }
